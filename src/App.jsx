672a69e8bf74f485272b7bde908890d0b36a8e43
maryamSalehi <maryam010salehi@gmail.com>
2025-09-21 14:42:30 +0330

    Added Filters component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { useGlobalStyles } from './styles/globals'
-import Header from './components/layout/header/Header';
+import { useGlobalStyles } from "./styles/globals";
+import Header from "./components/layout/header/Header";
+import Filters from "./components/filters/Filters";
 
 function App() {
   useGlobalStyles();
 
   return (
     <div>
-      <Header/>
+      <Header />
+      <main className="container">
+        <Filters />
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
