0f04838d03a716dddf4c5462791f60a92cdd8791
maryamSalehi <maryam010salehi@gmail.com>
2025-09-27 19:15:14 +0330

    Implemented infinite scroll logic

--- src/components/moviesList/MoviesList.jsx
+++ src/components/moviesList/MoviesList.jsx
@@ -1,20 +1,62 @@
 import { createUseStyles } from "react-jss";
-import MovieCard from "../movieCard/MovieCard"
+import MovieCard from "../movieCard/MovieCard";
 import { moviesListStyles } from "./moviesList.jss";
+import { useCallback, useEffect, useRef } from "react";
 
-const useStyles = createUseStyles(moviesListStyles)
+const useStyles = createUseStyles(moviesListStyles);
 
-function MoviesList({data}) {
+function MoviesList({ data, isLoading, hasMore, loadMore }) {
   const classes = useStyles();
+  const observer = useRef(null);
+
+  const lastItemRefCallback = useCallback(
+    (node) => {
+      if (isLoading) return;
+
+      if (observer.current) {
+        observer.current.disconnect();
+      }
+
+      if (!node) return;
+
+      observer.current = new IntersectionObserver(
+        (entries) => {
+          if (entries[0].isIntersecting && hasMore && !isLoading) {
+            loadMore();
+          }
+        },
+        {
+          rootMargin: "100px",
+          threshold: 0.1,
+        }
+      );
+
+      observer.current.observe(node);
+    },
+    [isLoading, hasMore, loadMore]
+  );
+
+  useEffect(() => {
+    return () => {
+      if (observer.current) {
+        observer.current.disconnect();
+      }
+    };
+  }, []);
+
   return (
     <div className={classes.moviesList}>
-      {
-        data?.map((movie) => (
-          <MovieCard movie={movie} key={movie.id}/>
-        ))
-      }
+      {data?.map((movie, index) => {
+        const isLastCard = index === data.length - 1;
+
+        return (
+          <div ref={isLastCard ? lastItemRefCallback : null} key={movie.id}>
+            <MovieCard movie={movie} />
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default MoviesList
+export default MoviesList;
