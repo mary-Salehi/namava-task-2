452a47bfc41cf421a26f0f550490b5378d3fa049
maryamSalehi <maryam010salehi@gmail.com>
2025-09-27 09:18:07 +0330

    Wrapped App into BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { theme } from './styles/theme.js'
-import { ThemeProvider } from 'react-jss'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { theme } from "./styles/theme.js";
+import { ThemeProvider } from "react-jss";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
