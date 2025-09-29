8a701580501b9944c773e57f33fb58615de81c13
maryamSalehi <maryam010salehi@gmail.com>
2025-09-29 14:41:19 +0330

    Implemented scrolling to top when page reloads

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { useGlobalStyles } from "./styles/globals";
 import Header from "./components/layout/header/Header";
 import BrowseSection from "./pages/browseSection/BrowseSection";
+import { useEffect } from "react";
 
 function App() {
   useGlobalStyles();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <div>
