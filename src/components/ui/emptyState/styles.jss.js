a8d1e4e1e232e11c4218afa1cd8fc6fd93312500
maryamSalehi <maryam010salehi@gmail.com>
2025-09-29 12:11:21 +0330

    Implemented an empty state ui

---
+++ src/components/ui/emptyState/styles.jss.js
@@ -0,0 +1,11 @@
+import { theme } from "../../../styles/theme";
+
+export const emptyStateStyles = {
+  emptyState: {
+    width: "400px",
+    textAlign: "center",
+    fontSize: "14px",
+    color: theme.colors.gray3,
+    margin: '0 auto',
+  },
+};
