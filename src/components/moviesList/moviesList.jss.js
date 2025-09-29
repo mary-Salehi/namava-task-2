4197b5b106db2383f2a494b880be5703fc95aa33
maryamSalehi <maryam010salehi@gmail.com>
2025-09-29 12:05:06 +0330

    Changed rowGap size

--- src/components/moviesList/moviesList.jss.js
+++ src/components/moviesList/moviesList.jss.js
@@ -2,15 +2,15 @@ import { theme } from "../../styles/theme";
 
 export const moviesListStyles = {
   moviesList: {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(3, 1fr)',
-    columnGap: '14px',
-    rowGap: '20px',
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 1fr)",
+    columnGap: "14px",
+    rowGap: "40px",
 
     [theme.mq(theme.breakpoints.desktop)]: {
-      gridTemplateColumns: 'repeat(5, 1fr)',
-      columnGap: '16px',
-    rowGap: '24px',
-    }
-  }
-}
+      gridTemplateColumns: "repeat(5, 1fr)",
+      columnGap: "16px",
+      rowGap: "70px",
+    },
+  },
+};
