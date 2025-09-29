3aa327b41c1c1d86a6b09a4a76231e3741278f38
maryamSalehi <maryam010salehi@gmail.com>
2025-09-29 11:55:13 +0330

    Created seperate component for brief preview

---
+++ src/components/movieCard/briefPreviewOverlay/styles.js
@@ -0,0 +1,35 @@
+import { theme } from "../../../styles/theme";
+
+export const briefPreviewStyles = {
+  overlay: {
+    opacity: ({ isHovered }) => (isHovered ? 1 : 0),
+    position: "absolute",
+    inset: 0,
+    background: "linear-gradient(to top, rgba(0,0,0,1), rgba(0,0,0,0.4))",
+    transition: "opacity 0.5s ease-in-out",
+  },
+
+  info: {
+    display: "flex",
+    flexDirection: "column",
+    position: "absolute",
+    bottom: "25px",
+    right: "10px",
+    lineHeight: "normal",
+    gap: "10px",
+  },
+
+  flexRow: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    gap: "4px",
+  },
+
+  icon: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    fill: theme.colors.white,
+  },
+};
