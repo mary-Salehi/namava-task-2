b6b10d39f2f01a203f86f8edc6d1c19809a08d9e
maryamSalehi <maryam010salehi@gmail.com>
2025-09-29 12:07:04 +0330

    Implemented seperate fetching logic for briefPreview

---
+++ src/hooks/useMoviePreview.js
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import { API_BASE } from "./useFetch";
+import axios from "axios";
+
+const cachedPreview = new Map();
+
+export const useMoviePreview = (movieId, isHovered) => {
+  const [briefPreview, setBriefPreview] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (!isHovered || !movieId) {
+      setBriefPreview(null);
+      return;
+    }
+
+    // Use cached data if exists
+    const cacheKey = movieId;
+    if (cachedPreview.has(cacheKey)) {
+      setBriefPreview(cachedPreview.get(cacheKey));
+      return;
+    }
+
+    const controller = new AbortController();
+
+    const fetchPreview = async () => {
+      try {
+        setIsLoading(true);
+        setError(null);
+
+        const response = await axios.get(
+          `${API_BASE}/v1.0/medias/${movieId}/brief-preview`,
+          {
+            signal: controller.signal,
+          }
+        );
+        // Cache data
+        cachedPreview.set(cacheKey, response.data.result);
+        setBriefPreview(response.data.result);
+      } catch (err) {
+        if (err.name !== "AbortError") {
+          setError(err);
+          console.error("Error fetching preview:", err);
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchPreview();
+
+    return () => {
+      controller.abort();
+    };
+  }, [movieId, isHovered]);
+
+  return { briefPreview, error, isLoading };
+};
