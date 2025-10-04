e6f8cac00b23d8dbc668c6e3925fa1d88531f0f8
maryamSalehi <maryam010salehi@gmail.com>
2025-10-04 12:52:22 +0330

    Added some coments

--- src/components/movieCard/index.jsx
+++ src/components/movieCard/index.jsx
@@ -1,15 +1,20 @@
 import { useStyles } from "./styles";
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { useDebounce } from "../../hooks/useDebounce";
 import BriefPreview from "./briefPreviewOverlay";
 import { useMediaPreview } from "../../hooks/useMediaPreview";
-
 
 function MovieCard({ movie }) {
   const classes = useStyles();
   const [isHovered, setIsHovered] = useState(false);
   const debouncedHover = useDebounce(isHovered, 300);
   const { briefPreview } = useMediaPreview(movie.id, debouncedHover);
+
+  // const { data: briefPreview } = useFetch(
+  //   debouncedHover ? `v1.0/medias/${movie.id}/brief-preview` : null,
+  //   {},
+  //   false
+  // );
 
   const handleMouseEnter = useCallback(() => setIsHovered(true), []);
   const handleMouseLeave = useCallback(() => setIsHovered(false), []);
@@ -22,7 +27,11 @@ function MovieCard({ movie }) {
         className={classes.imageCard}
       >
         {briefPreview && (
-            <BriefPreview movie={movie} briefPreview={briefPreview} isHovered={isHovered}/>
+          <BriefPreview
+            movie={movie}
+            briefPreview={briefPreview}
+            isHovered={isHovered}
+          />
         )}
         <img className={classes.movieImage} src={movie.image_url} alt="" />
       </div>
