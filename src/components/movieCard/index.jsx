db3313f4bf41d34dea4897b64b5f1a9b855929ff
maryamSalehi <maryam010salehi@gmail.com>
2025-10-04 08:09:32 +0330

    Changed useMoviePreview name and import path

--- src/components/movieCard/index.jsx
+++ src/components/movieCard/index.jsx
@@ -1,15 +1,15 @@
 import { useStyles } from "./styles";
 import { useCallback, useState } from "react";
-import { useMoviePreview } from "../../hooks/useMoviePreview";
 import { useDebounce } from "../../hooks/useDebounce";
 import BriefPreview from "./briefPreviewOverlay";
+import { useMediaPreview } from "../../hooks/useMediaPreview";
 
 
 function MovieCard({ movie }) {
   const classes = useStyles();
   const [isHovered, setIsHovered] = useState(false);
   const debouncedHover = useDebounce(isHovered, 300);
-  const { briefPreview } = useMoviePreview(movie.id, debouncedHover);
+  const { briefPreview } = useMediaPreview(movie.id, debouncedHover);
 
   const handleMouseEnter = useCallback(() => setIsHovered(true), []);
   const handleMouseLeave = useCallback(() => setIsHovered(false), []);
