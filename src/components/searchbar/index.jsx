23c58c194a895b89e8b5d963afdb48044e0cbe39
maryamSalehi <maryam010salehi@gmail.com>
2025-10-06 08:03:13 +0330

    Changed handlers logic

--- src/components/searchbar/index.jsx
+++ src/components/searchbar/index.jsx
@@ -4,35 +4,36 @@ import { useEffect, useRef } from "react";
 import DeleteIcon from "../icons/DeleteIcon";
 import SearchIcon from "../icons/SearchIcon";
 
-function SearchBar({ setSearchParams, searchQuery, type }) {
+function SearchBar({
+  setSearchParams,
+  searchQuery,
+  type,
+  setData,
+}) {
   const classes = useStyles();
   const inputRef = useRef(null);
 
   const handleValueChange = (e) => {
     const newQuery = e.target.value;
-    const params = {};
-    if (newQuery) {
-      params.query = newQuery;
+    if (!newQuery) {
+      setData([]);
     }
-
+    const params = {};
+    params.query = newQuery;
     if (type) {
       params.type = type;
     }
-
     setSearchParams(params);
-    console.log('after value change params are',params);
-    
   };
 
   const handleClearSearch = (e) => {
     e.preventDefault();
-
+    setData([]);
     const params = {};
     if (type) {
       params.type = type;
     }
     setSearchParams(params);
-        console.log('after clear search params are',params);
     if (inputRef.current) {
       inputRef.current.focus();
     }
